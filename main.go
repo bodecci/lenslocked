143e236fda31719ce3bd35dc4a49ceb67c9f62bd
Bode Falade <bbfalade@spscommerce.com>
2019-11-29 07:01:08 -0600

    Added a contact page.

--- main.go
+++ main.go
@@ -7,7 +7,14 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my  Super Awesome site!</h1>")
+	// fmt.Fprint(w, r.URL.Path) // shows the address path in the DOM
+
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my  Super Awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, `To get in touch, please send an email to <a
+		href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>`)
+	}
 }
 
 func main() {
