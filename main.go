add9209c262f4203db71aa8d34d5f6d83fe1fda1
Bode Falade <bbfalade@spscommerce.com>
2020-01-27 15:37:34 -0600

    added a contact page using conatc template.

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 )
 
 var homeTemplate *template.Template
+var contactTemplate *template.Template
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
@@ -20,8 +21,9 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 func contact(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, `To get in touch, please send an email to <a
-		href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>`)
+	if err := contactTemplate.Execute(w, nil); err != nil {
+		panic(err)
+	}
 
 }
 
@@ -43,7 +45,11 @@ func main() {
 
 	var err error
 	homeTemplate, err = template.ParseFiles("views/home.gohtml")
+	if err != nil {
+		panic(err)
+	}
 
+	contactTemplate, err = template.ParseFiles("views/contact.gohtml")
 	if err != nil {
 		panic(err)
 	}
